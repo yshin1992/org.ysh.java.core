8950d99c29a6ea0a7c512090c3f05d399eabb0ea
刘刘守春 <yshin1992@163.com>
2017-08-21 07:31:53 +0800

    Chapter5 ObjectAnalyzer处理数组显示不出值的bug

--- src/org/ysh/java/core/chapter5/ObjectAnalyzerSample.java
+++ src/org/ysh/java/core/chapter5/ObjectAnalyzerSample.java
@@ -1,8 +1,10 @@
 package org.ysh.java.core.chapter5;
 
 import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 /**
  * 通用toString()方法
@@ -14,7 +16,7 @@ public class ObjectAnalyzerSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "Hello";
-		String res = toString(str);
+		String res = toString2(str);
 		System.out.println(res);
 	}
 
@@ -55,5 +57,63 @@ public class ObjectAnalyzerSample {
 		}while(null != clazz);
 		return builder.toString();
 	}
+	
+	/**
+	 * 解决数组显示[C@5c647e05,hash=0][]的问题
+	 * @param obj
+	 * @return
+	 */
+	public static String toString2(Object obj) {
+		StringBuilder builder = new StringBuilder();
+		if(null == obj) {
+			return "null";
+		}
+		Class<?> clazz = obj.getClass();
+		builder.append(clazz.getName() + "");
+		do {
+			builder.append("[");
+			Field[] fs = clazz.getDeclaredFields();
+			AccessibleObject.setAccessible(fs, true);
+			for(Field f:fs) {
+				if(!Modifier.isStatic(f.getModifiers())) {
+					if(!builder.toString().endsWith("[")) {
+						builder.append(",");
+					}
+					builder.append(f.getName() + "=");
+					try {
+						Object val = f.get(obj);
+						Class<?> type = f.getType();
+						if(type.isPrimitive()) {
+							builder.append(val);
+						}else if(type.isArray()) {
+							Class<?> componentType = type.getComponentType();
+							builder.append(componentType.getName()+"[]{");
+							int arrLen = Array.getLength(val);
+							for(int i=0;i<arrLen;i++) {
+								Object arrVal = Array.get(val,i);
+								if(componentType.isPrimitive()) {
+									builder.append(arrVal);
+								}else {
+									//对于非原始类型暂时不做特殊处理
+									builder.append(arrVal);
+								}
+								if(i<arrLen -1) {
+									builder.append(",");
+								}
+							}
+							builder.append("}");
+						}
+						
+					}catch(Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			builder.append("]");
+			clazz = clazz.getSuperclass();
+		}while(null != clazz);
+		return builder.toString();
+	}
+	
 }
 
