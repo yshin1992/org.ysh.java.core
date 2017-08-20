9879677b309a52ebf57ff1ce34ce2695753c9d08
刘刘守春 <yshin1992@163.com>
2017-08-20 16:59:54 +0800

    Chapter5 Object实例

---
+++ src/org/ysh/java/core/chapter5/ObjectSample.java
@@ -0,0 +1,12 @@
+package org.ysh.java.core.chapter5;
+
+/**
+ * 除了基本类型外，Object是所有类的超类
+ * 用户在自定义类时，没有明确的指出父类时，那么Object就被认为是这个类的父类
+ * 另外数组的父类同样是Object
+ * @author liuliushouchun
+ *
+ */
+public class ObjectSample {
+
+}
