e7add69b275377397c5323336db3113392289918
刘刘守春 <yshin1992@163.com>
2017-08-20 17:12:27 +0800

    Chapter5 反射类的实例：域，构造函数和方法名

---
+++ src/org/ysh/java/core/chapter5/ReflectSample.java
@@ -0,0 +1,96 @@
+package org.ysh.java.core.chapter5;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class ReflectSample {
+	
+	public static void main(String[] args) {
+		Class<?> clazz = Double.class;
+//		printConstructors(clazz);
+		printFields(clazz);
+//		printMethods(clazz);
+	}
+	/**
+	 * 构造函数
+	 * @param clazz
+	 */
+	public static void printConstructors(Class<?> clazz) {
+		Constructor<?>[] constructors = clazz.getConstructors();
+		for(Constructor<?> cst : constructors) {
+			System.out.print(" ");
+			int modifiers = cst.getModifiers();
+			String mod = Modifier.toString(modifiers);
+			//修饰符
+			if(mod.length() > 0) {
+				System.out.print(mod + " ");
+			}
+			String name = cst.getName();
+			System.out.print(name + "(");
+			
+			Class<?>[] parameterTypes = cst.getParameterTypes();
+			if(null != parameterTypes && parameterTypes.length > 0) {
+				for(int i=0;i<parameterTypes.length;i++) {
+					Class<?> tmp = parameterTypes[i];
+					System.out.print(tmp.getTypeName());
+					if(i < parameterTypes.length - 1) {
+						System.out.print(",");
+					}
+				}
+				System.out.println(");");
+			}
+		}
+	}
+	
+	/**
+	 * 域
+	 * @param clazz
+	 */
+	public static void printFields(Class<?> clazz) {
+		Field[] fields = clazz.getFields();
+		if(null != fields && fields.length > 0) {
+			for(Field field : fields) {
+				System.out.print(" ");
+				String modifier = Modifier.toString(field.getModifiers());
+				if(modifier.length() > 0) {
+					System.out.print(modifier + " ");
+				}
+				String name = field.getName();
+				String type = field.getType().getTypeName();
+				System.out.println(type+ " " + name + ";");
+			}
+		}
+	}
+	
+	public static void printMethods(Class<?> clazz) {
+		Method[] methods = clazz.getMethods();
+		if(null != methods && methods.length > 0) {
+			for(Method m : methods) {
+				System.out.print(" ");
+				String returnType = m.getReturnType().getTypeName();
+				String modifier = Modifier.toString(m.getModifiers());
+				if(modifier.length() > 0) {
+					System.out.print(modifier + " ");
+				}
+				String methodName = m.getName();
+				System.out.print(returnType + " " + methodName+ "(");
+				
+				Class<?>[] parameterTypes = m.getParameterTypes();
+				if(null != parameterTypes && parameterTypes.length > 0) {
+					for(int i =0;i<parameterTypes.length;i++) {
+						String paramType = parameterTypes[i].getTypeName();
+						System.out.print(paramType);
+						if(i < parameterTypes.length - 1) {
+							System.out.print(",");
+						}
+					}
+				}
+				System.out.println(");");
+			}
+		}
+		
+		
+	}
+}
